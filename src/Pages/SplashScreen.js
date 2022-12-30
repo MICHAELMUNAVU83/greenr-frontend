814cfdf8867a9a4879427ca52ab9ef1953e1b830
Mercy <mercycheruiyot2013@gmail.com>
2022-12-30 12:07:03 +0300

    Added a Commit

--- src/Pages/SplashScreen.js
+++ src/Pages/SplashScreen.js
@@ -5,6 +5,7 @@ function SplashScreen() {
   return (
     <div>
       SplashScreen You will see this page if you are not logged in.
+      add a new page and test
       <Link to="/signup">Sign up</Link>
       <Link to="/login">Log in</Link>
     </div>
