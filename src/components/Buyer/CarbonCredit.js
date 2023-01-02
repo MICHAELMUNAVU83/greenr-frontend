f8b6fc4ba138a4bd9d6f09974ac7543c891a94cc
Realmunene <josep.mwangi@student.moringaschool.com>
2023-01-02 18:12:57 +0300

    make the height of the card to be adaptable

--- src/components/Buyer/CarbonCredit.js
+++ src/components/Buyer/CarbonCredit.js
@@ -8,11 +8,10 @@ function CarbonCredit(props) {
   {props.content.map((carbons)=>(
       <div className="Card">
       <img src={carbons.image} alt="image"/>
-      <Link>Victoria Island, <p>Lagos</p></Link>
+      <p><Link>{carbons.source}</Link></p>
       <p>At a price of ${carbons.price}, 10,000 {carbons.source} planted on 2 acres of land.</p>
       <p>Offsetting {carbons.amount} of C02</p>
       </div>
-      
     ))}
   </>
 )
