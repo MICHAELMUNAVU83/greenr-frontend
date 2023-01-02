2995f4eccfca90d9bca2c2ae4cf403e9936e21ed
Realmunene <josep.mwangi@student.moringaschool.com>
2023-01-02 15:58:53 +0300

    displayed the carbon cards

--- src/components/Buyer/CarbonCredit.js
+++ src/components/Buyer/CarbonCredit.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function CarbonCredit() {
-  return <div>CarbonCredit</div>;
+function CarbonCredit(props) {
+  return (
+  <>
+  {props.content.map((carbons)=>(
+      <div className="Card">
+      <img src={carbons.image} alt="image"/>
+      <Link>Victoria Island, <p>Lagos</p></Link>
+      <p>At a price of ${carbons.price}, 10,000 {carbons.source} planted on 2 acres of land.</p>
+      <p>Offsetting {carbons.amount} of C02</p>
+      </div>
+    ))}
+  </>
+)
 }
 
 export default CarbonCredit;
--- src/components/Buyer/CarbonCredits.js
+++ src/components/Buyer/CarbonCredits.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import CarbonCredit from "./CarbonCredit";
 
 function CarbonCredits() {
-  return <div>CarbonCredits</div>;
+const [carbonCard, setCarbonCard]=useState([]);
+useEffect(() => {
+  fetch("/api/v1/carbon_credits")
+  .then((resp)=> resp.json())
+  .then((data)=>{
+    setCarbonCard(data);
+  });
+}, []);
+  return <div>
+    <CarbonCredit content={carbonCard}/>
+  </div>;
 }
 
 export default CarbonCredits;
