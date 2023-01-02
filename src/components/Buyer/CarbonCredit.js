ced5101bae590f0157ba1fef0848ae3103afb6ea
Realmunene <josep.mwangi@student.moringaschool.com>
2023-01-02 21:38:47 +0300

    updated a fixed image

--- src/components/Buyer/CarbonCredit.js
+++ src/components/Buyer/CarbonCredit.js
@@ -7,7 +7,7 @@ function CarbonCredit(props) {
   <>
   {props.content.map((carbons)=>(
       <div className="Card">
-      <img src={carbons.image} alt="image"/>
+      <img src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" alt="image"/>
       <p><Link>{carbons.source}</Link></p>
       <p>At a price of ${carbons.price}, 10,000 {carbons.source} planted on 2 acres of land.</p>
       <p>Offsetting {carbons.amount} of C02</p>
