82fff5fb847a02718bf1059b2644c28a450b9f25
+ <+>
2023-01-03 10:19:42 +0300

    basic css

--- src/components/Buyer/EachCarbonCredit.js
+++ src/components/Buyer/EachCarbonCredit.js
@@ -21,7 +21,7 @@ const EachCarbonCredit = () => {
     <div className='carboncreditcard '>
         <img className='img' src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" alt="image"/>
         <div className='carbondetails'>
-            <button type="button" class="btn btn-secondary">{carbon.price}</button>
+            <button type="button" className="btn btn-outline-success text-center">{carbon.price}</button>
             <p className='carbonparagraph'>As trees grow, they take in carbon from the air and store it in wood, plant matter, and in the soil,
                making them what scientists call “carbon sinks.”
                In this way, forests play an important role in the global carbon
