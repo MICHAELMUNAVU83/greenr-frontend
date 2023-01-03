0f3fca9d0f8c91042124986526ad64fc03459e42
+ <+>
2023-01-03 09:49:10 +0300

    polishing css

--- src/components/Buyer/EachCarbonCredit.js
+++ src/components/Buyer/EachCarbonCredit.js
@@ -1,5 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import { useNavigate, useParams } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './EachCarbonCredit.css';
 
 
 const EachCarbonCredit = () => {
@@ -16,15 +18,16 @@ const EachCarbonCredit = () => {
     
 
   return (
-    <div className='carboncreditcard my-5'>
-        <img src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" alt="image"/>
+    <div className='carboncreditcard '>
+        <img className='img' src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80" alt="image"/>
         <div className='carbondetails'>
-            <button>{carbon.price}</button>
-            <p>As trees grow, they take in carbon from the air and store it in wood, plant matter, and in the soil,
+            <button type="button" class="btn btn-secondary">{carbon.price}</button>
+            <p className='paragraph'>As trees grow, they take in carbon from the air and store it in wood, plant matter, and in the soil,
                making them what scientists call “carbon sinks.”
                In this way, forests play an important role in the global carbon
                cycle by soaking up lots of carbon dioxide that would otherwise live in the atmosphere.
             </p>
+            <button type="button" class="btn btn-success">Buy Now</button>
         </div>
         
     </div>
