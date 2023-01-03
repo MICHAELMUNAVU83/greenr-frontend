fb01f5b9a8118a066ec36cb799247ccff0138d04
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-03 11:54:11 +0300

    Created navbars

--- src/components/NavBar/BuyerNavbar.js
+++ src/components/NavBar/BuyerNavbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../Images/greenrlogo.png";
+import headerlogo from "../Images/headerlogo.png";
 
 function BuyerNavbar({ setStoredToken }) {
   return (
     <div>
-      <img src={logo} alt="logo" />
+      <img src={headerlogo} alt="headerlogo" />
 
       <Link to="/">Home</Link>
       <Link to="/carboncredits">Buy Credits</Link>
