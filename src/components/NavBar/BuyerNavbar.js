51223336adcdd344f5602d8e6d99d88c8e2b4572
Mercy <mercycheruiyot2013@gmail.com>
2023-01-04 13:59:41 +0300

    BuyerNavbar style1

--- src/components/NavBar/BuyerNavbar.js
+++ src/components/NavBar/BuyerNavbar.js
@@ -5,12 +5,29 @@ import headerlogo from "../Images/headerlogo.png";
 function BuyerNavbar({ setStoredToken }) {
   const navigate = useNavigate();
   return (
-    <div>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
       <img src={headerlogo} alt="headerlogo" />
+      <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarButtonsExample"
+      aria-controls="navbarButtonsExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <ul className="navbar-nav">
+      <li className="nav-item"><Link className="nav-link" to="/" >Home</Link> </li>
+      <li className="nav-item"><Link className="nav-link" to="/carboncredits">Buy Credits</Link></li>
+      <li className="nav-item"><Link className="nav-link" to="/mypurchases">My Purchases</Link></li>
 
-      <Link to="/">Home</Link>
+      {/* <Link to="/">Home</Link>
       <Link to="/carboncredits">Buy Credits</Link>
-      <Link to="/mypurchases">My Purchases</Link>
+      <Link to="/mypurchases">My Purchases</Link> */}
+    </ul>
 
       <button
         onClick={() => {
@@ -23,7 +40,8 @@ function BuyerNavbar({ setStoredToken }) {
       >
         Log out
       </button>
-    </div>
+      </div>
+    </nav>
   );
 }
 
