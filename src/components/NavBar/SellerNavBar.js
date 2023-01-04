70a606d33bad4b7f0dd4c63b651007fe97aa7cec
Mercy <mercycheruiyot2013@gmail.com>
2023-01-04 16:47:52 +0300

    Navbar styling fr seller

--- src/components/NavBar/SellerNavBar.js
+++ src/components/NavBar/SellerNavBar.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 import headerlogo from "../Images/headerlogo.png";
 
 function SellerNavbar({ setStoredToken }) {
   const navigate = useNavigate();
   return (
-    <div>
+    <nav className="navbar navbar-expand-lg bg-light">
+    <div className="container-fluid">
       <img src={headerlogo} alt="headerlogo" />
-
-      <Link to="/">Home</Link>
-      <Link to="/mycarboncredits">My Carbon Credits</Link>
-      <Link to="/newcarboncredit">Add New Carbon Credit</Link>
+      <ul className="nav nav-fill" >
+        <li className="nav-item ">
+          <Link className="nav-link green-active" data-mdb-toggle="pill" to="/" >Home</Link>
+        </li>
+        <li className="nav-item ">
+          <Link className="nav-link green-active" to="/mycarboncredits">My Carbon Credits</Link>
+          </li>
+        <li className="nav-item ">
+          <Link className="nav-link green-active" to="/newcarboncredit">Add New Carbon Credit</Link>
+          </li>
+      </ul>
+      
+      
       <button
         onClick={() => {
           navigate("/");
@@ -22,7 +33,8 @@ function SellerNavbar({ setStoredToken }) {
       >
         Log out
       </button>
-    </div>
+      </div>
+    </nav>
   );
 }
 
