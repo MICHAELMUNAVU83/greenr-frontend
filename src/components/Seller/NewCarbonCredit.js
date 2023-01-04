69b07bf73a065552a54913627dae6aae9d7117b6
+ <+>
2023-01-04 10:37:16 +0300

    reset the post setup

--- src/components/Seller/NewCarbonCredit.js
+++ src/components/Seller/NewCarbonCredit.js
@@ -1,29 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const initialState = {
-  amount: "",
-  image: "",
-  price: "",
-  source: "",
-};
-
 function NewCarbonCredit({ loggedInUserId }) {
-  const [formData, setFormData] = useState("initialState");
+
   const [image, setImage] = useState('')
+  const [amount, setAmount] = useState('')
+  const [price, setPrice] = useState('')
+  const [source, setSource] = useState('')
+
   const navigate = useNavigate();
-
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialState);
-  }
 
   const uploadImage = (files) => {
     const formData = new FormData();
@@ -31,52 +16,69 @@ function NewCarbonCredit({ loggedInUserId }) {
     formData.append("upload_preset", "e2e6z2lx");
     fetch("https://api.cloudinary.com/v1_1/dakiak4mc/image/upload", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify (formData),
+      body: formData,
     })
-    setFormData({
-      amount:'',
-      image: '',
-      price: '',
-      source:'',
-      user_id: {loggedInUserId} ,
-  })
-  navigate("/carboncredits")
-      .then((response) => response.json())
-      .then((data) => {
-        setImage(data.secure_url);
+    .then((response) => response.json())
+    .then((data) => {
+      setImage(data.secure_url);
+    });
+};
+
+
+    const addACarbonCredit = (e) => {
+      e.preventDefault();
+      fetch("/api/v1/carbon_credits", {
+        method: "POST",
+        headers: {
+          Accepts: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          image: image,
+          amount: amount,
+          price: price,
+          source: source,
+        }),
       });
-  };
+  
+      setTimeout(() => {
+        navigate("/carboncredits");
+      }, 1000);
+    };
   return (
     <div>
       <p className="title">Add New Carbon Project</p>
-      <form className="signup-form1"  onSubmit={handleSubmit}>
+      <form className="signup-form1">
         <p>Fill in the details below to add a new carbon project</p>
         <label>Source of carbon offset</label>
         <input
           type="text"
           className="form-control signup-input"
           placeholder="eg Artificial forest with 1000 trees"
-          value={formData.source}
-          onChange={handleChange}
+          value={source}
+          onChange={(e) => {
+            setSource(e.target.value);
+          }}
         />
         <label>Amount of carbon offset by the project</label>
         <input
           type="text"
           className="form-control signup-input"
           placeholder="Amount in kgs of Co2"
-          value={formData.amount}
-          onChange={handleChange}
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
         />
         <label>Price of carbon credits</label>
         <input
           type="text"
           className="form-control signup-input"
           placeholder="Amount in kgs of CO2"
-          value={formData.price}
-          onChange={handleChange}
+          value={price}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
         />
         <label>Project Image</label> <br />
         <input
@@ -85,10 +87,9 @@ function NewCarbonCredit({ loggedInUserId }) {
           }}
           type="file"
           className="form-control signup-input"
-          value={formData.image}
     
         />
-        <button className="signup-button">Submit</button>
+        <button className="signup-button" onClick={addACarbonCredit}>Submit</button>
       </form>
     </div>
   );
