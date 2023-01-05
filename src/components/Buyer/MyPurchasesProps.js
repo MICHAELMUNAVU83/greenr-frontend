8a25d2ed77988f5d8291a9dd3df916536b9eacad
+ <+>
2023-01-05 10:10:57 +0300

    set the useeffect with user logged in

--- src/components/Buyer/MyPurchasesProps.js
+++ src/components/Buyer/MyPurchasesProps.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import MyPurchases from './MyPurchases'
 import './MyPurchases.css';
 
-const MyPurchasesProps = () => {
+const MyPurchasesProps = ({ loggedInUserId }) => {
     const [purchasesCard, setPurchasesCard]=useState([]);
+
+    useEffect(() => {
+        fetch(`/api/v1/my_purchases/${loggedInUserId}`)
+        .then((resp)=> resp.json())
+        .then((data)=>{
+          setPurchasesCard(data);
+        });
+      }, []);
+
   return (
     <div className="mypurchases">
         <MyPurchases />
