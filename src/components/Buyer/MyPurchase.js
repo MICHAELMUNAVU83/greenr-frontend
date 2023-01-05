bc01bfc96f17b936c517e7cac3ed7da5cccae78a
+ <+>
2023-01-05 12:46:05 +0300

    polishing up on the css

--- src/components/Buyer/MyPurchase.js
+++ src/components/Buyer/MyPurchase.js
@@ -13,10 +13,10 @@ const MyPurchase = ({ content }) => {
           />
            <p className="p6">{purchase.carbon_credit.source}</p>
           <p className="p1"> <IoLocationOutline />{purchase.buyer_location}</p>
-          <p className="p2">Phone No. {purchase.buyer_phone_number}</p>
-          <p  className="p3">Amount {purchase.carbon_credit.amount}</p>
-          <p  className="p4">Location {purchase.carbon_credit.location}</p>
-          <p className="p5">Price {purchase.carbon_credit.price}</p>
+          <p className="p2">Phone No: {purchase.buyer_phone_number}</p>
+          <p  className="p3">Amount: {purchase.carbon_credit.amount}</p>
+          <p  className="p4">Location: {purchase.carbon_credit.location}</p>
+          <p className="p5">Price: {purchase.carbon_credit.price}</p>
         </div>
       ))}
     </>
