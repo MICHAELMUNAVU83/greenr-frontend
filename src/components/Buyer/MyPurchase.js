0b7ce4e9b382cfe8605a3356280e8d4f80225c4b
+ <+>
2023-01-05 12:38:40 +0300

    simple polishing of the mapped data

--- src/components/Buyer/MyPurchase.js
+++ src/components/Buyer/MyPurchase.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./MyPurchases.css";
+import { IoLocationOutline } from "react-icons/io5";
 
 const MyPurchase = ({ content }) => {
   return (
@@ -10,12 +11,12 @@ const MyPurchase = ({ content }) => {
             src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
             alt="image"
           />
-          <p>{purchase.buyer_location}</p>
-          <p>{purchase.buyer_phone_number}</p>
-          <p>{purchase.carbon_credit.amount}</p>
-          <p>{purchase.carbon_credit.location}</p>
-          <p>{purchase.carbon_credit.price}</p>
-          <p>{purchase.carbon_credit.source}</p>
+           <p className="p6">{purchase.carbon_credit.source}</p>
+          <p className="p1"> <IoLocationOutline />{purchase.buyer_location}</p>
+          <p className="p2">Phone No. {purchase.buyer_phone_number}</p>
+          <p  className="p3">Amount {purchase.carbon_credit.amount}</p>
+          <p  className="p4">Location {purchase.carbon_credit.location}</p>
+          <p className="p5">Price {purchase.carbon_credit.price}</p>
         </div>
       ))}
     </>
