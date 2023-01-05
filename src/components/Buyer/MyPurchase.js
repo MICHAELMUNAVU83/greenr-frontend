8cad3d348a22ee33c3613e9fd7dc6b3152cb1529
+ <+>
2023-01-05 11:32:57 +0300

    mapped through the objects

--- src/components/Buyer/MyPurchase.js
+++ src/components/Buyer/MyPurchase.js
@@ -10,7 +10,12 @@ const MyPurchase = ({ content }) => {
             src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
             alt="image"
           />
-          <p>inm</p>
+          <p>{purchase.buyer_location}</p>
+          <p>{purchase.buyer_phone_number}</p>
+          <p>{purchase.carbon_credit.amount}</p>
+          <p>{purchase.carbon_credit.location}</p>
+          <p>{purchase.carbon_credit.price}</p>
+          <p>{purchase.carbon_credit.source}</p>
         </div>
       ))}
     </>
