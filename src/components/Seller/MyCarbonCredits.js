c418cf287c0068d185b6bb95250c5263e8f88d05
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-06 10:47:14 +0300

    fetched my carbon credits

--- src/components/Seller/MyCarbonCredits.js
+++ src/components/Seller/MyCarbonCredits.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function MyCarbonCredits() {
+function MyCarbonCredits({ loggedInUserId }) {
+  const [carbonCredits, setCarbonCredits] = useState([]);
+  useEffect(() => {
+    fetch(`/api/v1/my_carbon_credits/${loggedInUserId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCarbonCredits(data);
+      });
+  }, []);
   return <div>MyCarbonCredits</div>;
 }
 
