9def8fa9465b9470767a8cbdd11ff2ed94407b12
Realmunene <josep.mwangi@student.moringaschool.com>
2023-01-06 11:58:01 +0300

    added the card element

--- src/components/Seller/MyCarbonCredits.js
+++ src/components/Seller/MyCarbonCredits.js
@@ -12,7 +12,12 @@ function MyCarbonCredits({ loggedInUserId }) {
   return <div>
      <p className="title">My Carbon Credits</p>
      {carbonCredits.map((credit)=>(
-      
+      <div className="Card">
+      <img src={credit.image} alt="image"/>
+      <p><Link>{credit.source}</Link></p>
+      <p>At a price of ${credit.price}, 10,000 {credit.source} planted on 2 acres of land.</p>
+      <p>Offsetting {credit.amount} of C02</p>
+      </div>
     ))}
   </div>;
 }
