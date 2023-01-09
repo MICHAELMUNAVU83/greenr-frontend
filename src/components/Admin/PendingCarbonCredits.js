9af2eae1d2e173ae380872c6e6d2547406bc1d35
+ <+>
2023-01-09 13:12:07 +0300

    fetched data from api suing useffect & set state

--- src/components/Admin/PendingCarbonCredits.js
+++ src/components/Admin/PendingCarbonCredits.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PendingCarbonCredits = () => {
   return (
-    <div>PendingCarbonCredits</div>
+    <div cla>
+      PendingCarbonCredits
+
+    </div>
   )
 }
 
---
+++ src/components/Admin/PendingCarbons.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './PendingCarbonCredits.css';
+import PendingCarbonCredits from './PendingCarbonCredits';
+
+const PendingCarbons = () => {
+
+  const [pendingCard, setPendingCard] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/v1/carbon_credits")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPendingCard(data);
+      });
+  }, []);
+
+  return (
+    <div>
+        PendingCarbons
+    </div>
+  )
+}
+
+export default PendingCarbons
