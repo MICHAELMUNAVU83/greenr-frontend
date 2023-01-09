1364b33fe3a22fc6f9c0e8c4398de6bc34e9f33c
+ <+>
2023-01-09 14:46:26 +0300

    updated the more details functionality & styled

--- src/components/Admin/PendingCarbons.js
+++ src/components/Admin/PendingCarbons.js
@@ -14,10 +14,15 @@ const PendingCarbons = ({ content }) => {
           src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
           alt="image"
         /> 
-        <p className="p1">{carbonCredit.source}</p>
+        <p className="p1"><Link>{carbonCredit.source}</Link></p>
         <p className="p2">  At a price of ${carbonCredit.price}, 10,000 {carbonCredit.source} planted
            on 2 acres of land.</p>
         <p  className="p3">Offsetting {carbonCredit.amount} of C02</p>
+        <Link
+          to={`/eachcarboncredit/${carbonCredit.id}`}
+         >
+         More details
+       </Link>
       </div>
     ))}
     </>
