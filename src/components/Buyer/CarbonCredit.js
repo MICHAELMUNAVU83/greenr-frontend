e5712a56df80c468bf3374059e75c66b5b8bfb32
benaiah-ke <benaiahpraise@gmail.com>
2023-01-19 00:18:08 +0300

    use credit image on card

--- src/components/Buyer/CarbonCredit.js
+++ src/components/Buyer/CarbonCredit.js
@@ -14,7 +14,7 @@ function CarbonCredit(props) {
           carbons.approved === true && (
             <div className="Card">
               <img
-                src="https://images.unsplash.com/photo-1503785640985-f62e3aeee448?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+                src= {carbons.image}
                 alt="image"
               />
               <p>
