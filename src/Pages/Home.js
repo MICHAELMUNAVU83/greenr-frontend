b84e8746352459412ed2de42d0467710f8854c7b
benaiah-ke <benaiahpraise@gmail.com>
2023-01-19 01:27:55 +0300

    homepage styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,16 +2,23 @@ import React from "react";
 
 function Home({ setStoredToken }) {
   return (
-    <div className="container" style={{height:'65vh'}}>
-      <span style={{FontWeight:'700'}}>Welcome to Greenr!</span>Thank you for joining us at the forefront of the fight for our planet. You are logged into your account.
+    <div className="container" style={{height:'65vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+      <p style={{textAlign:'center', fontSize:'35px'}}>
+      <span style={{FontWeight:'700', display:'block', paddingBottom:'20px'}}>Welcome to Greenr! </span>Thank you for joining us at the forefront of the fight for our planet. You are logged into your account.
+      </p>
       <button
+        className="btn"
+
         onClick={() => {
           localStorage.setItem("token", "");
           setStoredToken("");
         }}
+
+        style={{width:'max-content', outline: 'none', padding:' 10px 20px 10px', marginTop:'30px', backgroundColor:'green', color:'white', borderRadius:'0px'}}
       >
         Log out
       </button>
+
     </div>
   );
 }
