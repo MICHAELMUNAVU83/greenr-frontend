d6eedeb9ef29186a15a2cdb2210f3c7648661212
benaiah-ke <benaiahpraise@gmail.com>
2023-01-18 21:59:31 +0300

    style seller navigation bar

--- src/components/NavBar/SellerNavBar.js
+++ src/components/NavBar/SellerNavBar.js
@@ -9,34 +9,27 @@ function SellerNavbar({ setStoredToken }) {
     <nav className="navbar navbar-expand-lg bg-light">
     <div className="container">
       <img src={headerlogo} alt="headerlogo" className="brand"/>
-      <div className="nav-links">
-      <ul className="nav nav-fill" >
-        <li className="nav-item ">
-          <Link className="nav-link green-active" to="/" >Home</Link>
-        </li>
-        <li className="nav-item ">
-          <Link className="nav-link green-active" to="/mycarboncredits">My Carbon Credits</Link>
-          </li>
-        <li className="nav-item ">
-          <Link className="nav-link green-active"  to="/newcarboncredit">Add New Carbon Credit</Link>
-          </li>
-      </ul>
-      
-      
-      <button
-      className="text-white btn border-none"
-        onClick={() => {
-          navigate("/");
-          setTimeout(() => {
-            localStorage.setItem("token", "");
-            setStoredToken("");
-          }, 1000);
-        }}
-      >
-        Log out
-      </button>
+
+      <div className="navbar-links">
+        <ul className="nav nav-fill" >
+          <li className="nav-item "><Link className="nav-link green-active" data-mdb-toggle="pill" to="/" >Home</Link> </li>
+          <li className="nav-item "><Link className="nav-link green-active " data-mdb-toggle="pill" to="/mycarboncredits">My Carbon Credits</Link></li>
+          <li className="nav-item "><Link className="nav-link green-active " data-mdb-toggle="pill" to="/newcarboncredit">Add New Carbon Credits</Link></li>
+        </ul> 
+        <button
+        className="text-white btn border-none"
+          onClick={() => {
+            navigate("/");
+            setTimeout(() => {
+              localStorage.setItem("token", "");
+              setStoredToken("");
+            }, 1000);
+          }}
+        >
+          Log out
+        </button>
       </div>
-      </div>
+    </div>
     </nav>
   );
 }
